0051ecd1bccdf92b5e62fd3f63d8439d78ca5de4
Meghan09 <104902070+Meghan09@users.noreply.github.com>
2022-07-26 21:28:52 +0530

    Update time.py

--- time.py
+++ time.py
@@ -1,5 +1,3 @@
-from datetime import *
-import pytz
-tz_INDIA = pytz.timezone('Asia/Kolkata') 
-datetime_INDIA = datetime.now(tz_INDIA)
-print("INDIA time:", datetime_INDIA.strftime("%H:%M:%S"))
+from datetime import date
+today = date.today()
+print("Today's date is", today)
